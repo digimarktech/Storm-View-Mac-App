331efdd61f857902fc238ae3580153388575182d
Marc Aupont <marc@digimarktech.com>
2017-06-03 00:31:52 -0400

    Initial Commit

---
+++ Project 1 Storm Viewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project 1 Storm Viewer
+//
+//  Created by Marc Aupont on 6/3/17.
+//  Copyright © 2017 Digimark Technical Solutions. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Project 1 Storm Viewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project 1 Storm Viewer
+//
+//  Created by Marc Aupont on 6/3/17.
+//  Copyright © 2017 Digimark Technical Solutions. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
